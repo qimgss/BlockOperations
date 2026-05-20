817f1480b014d3f18e127ad9272423b530d84f1e
qimgss <qimgss@126.com>
2026-05-20 18:39:17 +0800

    修改构建脚本

--- build.sh
+++ build.sh
@@ -5,12 +5,7 @@ exit
 echo "What are you going to do?"
 echo "1.Just clean"
 echo "2.Just build"
-echo "3.Build and copy to QSTools(main)"
-echo "4.Build and copy+push to QSTools(main)"
-echo "5.Clean and build"
-echo "6.Clean and build and copy to QSTools(main)"
-echo "7.Clean and build and copy+push to QSTools(main)"
-
+echo "3.Clean and build"
 read CleanInputs
 case $CleanInputs in
     1)
@@ -20,33 +15,6 @@ case $CleanInputs in
     cargo build --release && echo "build successful" || abort "build failed"
     ;;
     3)
-    cargo build --release && echo "build successful" || abort "build failed"
-    cp -af ./target/release/blkops ./../main/binary/blkops
-    ;;
-    4)
-    cargo build --release && echo "build successful" || abort "build failed"
-    cp -af ./target/release/blkops ./../main/binary/blkops
-    cd ../main
-    git add binary/
-    git commit -m "Update the blkops"
-    git push
-    ;;
-    5)
     cargo clean
     cargo build --release && echo "build successful" || abort "build failed"
-    ;;
-    6)
-    cargo clean
-    cargo build --release && echo "build successful" || abort "build failed"
-    cp -af ./target/release/blkops ./../main/binary/blkops
-    ;;
-    7)
-    cargo clean
-    cargo build --release && echo "build successful" || abort "build failed"
-    cp -af ./target/release/blkops ./../main/binary/blkops
-    cd ../main
-    git add binary/
-    git commit -m "Update the blkops"
-    git push
-    ;;
 esac
